e9c0f8c3069fd5ab1797e8daa06a58140522e0dc
arkanadli <uzzomaki@gmail.com>
2023-12-09 21:48:36 +0700

    Update usecases.dart

--- lib/core/usecases/usecases.dart
+++ lib/core/usecases/usecases.dart
@@ -1,5 +1,4 @@
-abstract class UseCase<Type,Params> {
+abstract class UseCase<Type, Params> {
+  // create a bigger scope of usecase, that will call the functional programm into some boilerplate
   Future<Type> call({Params params});
-
-  
-} 
+}
